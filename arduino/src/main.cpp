54cbc5b1d1b92191588508bd382d36a62b7acb89
Michael Kugler <michael@appdafuer.com>
2019-08-22 21:16:07 +0200

    3x NFC

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,176 +1,98 @@
 #include <Arduino.h>
+/*
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ */
 
-#define DASH_BUTTON_1 36
-#define LED_DASH_BUTTON_1 38
-#define SEGMENT_DISPLAY_1_DOT 2
-#define SEGMENT_DISPLAY_1_MIDDLE 35     // yellow
-#define SEGMENT_DISPLAY_1_UP_LEFT 33    // blue
-#define SEGMENT_DISPLAY_1_UP 31         // green
-#define SEGMENT_DISPLAY_1_UP_RIGHT 29   // red
-#define SEGMENT_DISPLAY_1_DOWN_LEFT 27  // black
-#define SEGMENT_DISPLAY_1_DOWN 25       // orange
-#define SEGMENT_DISPLAY_1_DOWN_RIGHT 23 // white
+#include <SPI.h>
+#include <MFRC522.h>
 
-int dashButton1 = 0;
-int count = 0;
+// #define RST_PIN 43 // SPI Reset Pin
+// #define SS_PIN 42  // SPI Slave Select Pin
 
-void showCount()
-{
+#define RST_PIN_2 45 // SPI Reset Pin
+#define SS_PIN_2 44  // SPI Slave Select Pin
 
-  int countTo10 = count % 10;
+// #define RST_PIN_3 49 // SPI Reset Pin
+// #define SS_PIN_3 48  // SPI Slave Select Pin
 
-  switch (countTo10)
-  {
-  case 0:
+// MFRC522 mfrc522(SS_PIN, RST_PIN); // Instanz des MFRC522 erzeugen
 
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, LOW);
+MFRC522 mfrc522_2(SS_PIN_2, RST_PIN_2); // Instanz des MFRC522 erzeugen
 
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-    break;
-  case 1:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, LOW);
-
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-
-    break;
-  case 2:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, LOW);
-
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, HIGH);
-    break;
-
-  case 3:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, LOW);
-
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, HIGH);
-    break;
-
-  case 4:
-
-    digitalWrite(SEGMENT_DISPLAY_1_UP, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, LOW);
-
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-
-    break;
-
-  case 5:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, LOW);
-
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-    break;
-
-  case 6:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, HIGH);
-    break;
-
-  case 7:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, LOW);
-    break;
-
-  case 8:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, HIGH);
-    break;
-
-  case 9:
-    digitalWrite(SEGMENT_DISPLAY_1_UP_LEFT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_RIGHT, HIGH);
-
-    digitalWrite(SEGMENT_DISPLAY_1_MIDDLE, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP_RIGHT, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_UP, HIGH);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN_LEFT, LOW);
-    digitalWrite(SEGMENT_DISPLAY_1_DOWN, HIGH);
-    break;
-
-  default:
-    break;
-  }
-}
+// MFRC522 mfrc522_3(SS_PIN_3, RST_PIN_3); // Instanz des MFRC522 erzeugen
 
 void setup()
 {
+  // Diese Funktion wird einmalig beim Start ausgeführt
   Serial.begin(9600); // Serielle Kommunikation mit dem PC initialisieren
-  Serial.print("Hello Button");
-
-  pinMode(DASH_BUTTON_1, INPUT_PULLUP);
-  pinMode(LED_DASH_BUTTON_1, OUTPUT);
-
-  pinMode(SEGMENT_DISPLAY_1_DOT, OUTPUT);
-  pinMode(SEGMENT_DISPLAY_1_MIDDLE, OUTPUT);
-  pinMode(SEGMENT_DISPLAY_1_UP_LEFT, OUTPUT);
-  pinMode(SEGMENT_DISPLAY_1_UP, OUTPUT);
-  pinMode(SEGMENT_DISPLAY_1_UP_RIGHT, OUTPUT);
-  pinMode(SEGMENT_DISPLAY_1_DOWN_LEFT, OUTPUT);
-  pinMode(SEGMENT_DISPLAY_1_DOWN, OUTPUT);
-  pinMode(SEGMENT_DISPLAY_1_DOWN_RIGHT, OUTPUT);
-
-  // analogWrite(SEGMENT_DISPLAY_1_DOT, 240);
-  analogWrite(SEGMENT_DISPLAY_1_DOT, 255);
-  digitalWrite(LED_DASH_BUTTON_1, HIGH);
-  
-  showCount();
+  SPI.begin();        // Initialisiere SPI Kommunikation
+  // mfrc522.PCD_Init();   // Initialisiere MFRC522 Lesemodul
+  mfrc522_2.PCD_Init(); // Initialisiere MFRC522 Lesemodul
+  // mfrc522_3.PCD_Init(); // Initialisiere MFRC522 Lesemodul
+  Serial.print("Hello 3");
 }
 
 void loop()
 {
+  // Diese Funktion wird in Endlosschleife ausgeführt
 
-  dashButton1 = digitalRead(DASH_BUTTON_1);
+  // Nur wenn eine Karte gefunden wird und gelesen werden konnte, wird der Inhalt von IF ausgeführt
+  // PICC = proximity integrated circuit card = kontaktlose Chipkarte
+  // if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
+  // {
+  //   Serial.print("Gelesene UID:");
+  //   for (byte i = 0; i < mfrc522.uid.size; i++)
+  //   {
+  //     // Abstand zwischen HEX-Zahlen und führende Null bei Byte < 16
+  //     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+  //     Serial.print(mfrc522.uid.uidByte[i], HEX);
+  //   }
+  //   Serial.println();
 
-  if (dashButton1 == LOW)
+  //   // Versetzt die gelesene Karte in einen Ruhemodus, um nach anderen Karten suchen zu können.
+  //   mfrc522.PICC_HaltA();
+  //   delay(1000);
+  // }
+
+  if (mfrc522_2.PICC_IsNewCardPresent() && mfrc522_2.PICC_ReadCardSerial())
   {
-    count++;
-    showCount();
-    delay(150);
+    Serial.print("Gelesene UID 2:");
+    for (byte i = 0; i < mfrc522_2.uid.size; i++)
+    {
+      // Abstand zwischen HEX-Zahlen und führende Null bei Byte < 16
+      Serial.print(mfrc522_2.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      Serial.print(mfrc522_2.uid.uidByte[i], HEX);
+    }
+    Serial.println();
+
+    // Versetzt die gelesene Karte in einen Ruhemodus, um nach anderen Karten suchen zu können.
+    mfrc522_2.PICC_HaltA();
+    delay(1000);
   }
-  delay(10);
+
+  // if (mfrc522_3.PICC_IsNewCardPresent() && mfrc522_3.PICC_ReadCardSerial())
+  // {
+  //   Serial.print("Gelesene UID 3:");
+  //   for (byte i = 0; i < mfrc522_3.uid.size; i++)
+  //   {
+  //     // Abstand zwischen HEX-Zahlen und führende Null bei Byte < 16
+  //     Serial.print(mfrc522_3.uid.uidByte[i] < 0x10 ? " 0" : " ");
+  //     Serial.print(mfrc522_3.uid.uidByte[i], HEX);
+  //   }
+  //   Serial.println();
+
+  //   // Versetzt die gelesene Karte in einen Ruhemodus, um nach anderen Karten suchen zu können.
+  //   mfrc522_3.PICC_HaltA();
+  //   delay(1000);
+  // }
 }
